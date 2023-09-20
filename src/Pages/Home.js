99a8d2ca5bcf668ffd5e5fa4b68cfd410d727a2f
RAMA KRISHNA <ramakrishnavema@outlook.com>
2023-09-20 18:20:57 +0530

    home layout component created and images added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,136 @@
 import React from 'react'
+import '../Css/Home.css'
+import veg from '../Images/Home/Rectangle 1403.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="home">
+  <div className="group-2227">
+    <img className="rectangle-1403" src={veg}/>
+  </div>
+  <div className="frame-19">
+    <div className="frame-20">
+      <div className="best-sellers">Daily Needs</div>
+      <div className="group-2225">
+        <div className="rectangle-1410">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Vegetables</div>
+          </div>
+        </div>
+        <div className="rectangle-1411">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Milk</div>
+          </div>
+        </div>
+        <div className="rectangle-1412">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">News Papers</div>
+          </div>
+        </div>
+        <div className="rectangle-1413">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Curd</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="frame-192">
+      <div className="best-sellers">Best Sellers</div>
+      <div className="group-2225">
+        <div className="rectangle-1410">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Milk</div>
+          </div>
+        </div>
+        <div className="rectangle-1411">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Curd</div>
+          </div>
+        </div>
+        <div className="rectangle-1412">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Cookies</div>
+          </div>
+        </div>
+        <div className="rectangle-1413">
+          <div className="frame-30">
+            <img className="rectangle-14102" src="rectangle-14102.png" />
+            <div className="name">Eggs</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="frame-21">
+      <div className="best-sellers">Brands</div>
+      <div className="group-22252">
+        <div className="rectangle-1429">
+          <img className="rectangle-1428" src="rectangle-1428.png" />
+        </div>
+        <div className="rectangle-1430">
+          <img className="rectangle-1428" src="rectangle-1428.png" />
+        </div>
+        <div className="rectangle-1431">
+          <img className="rectangle-1428" src="rectangle-1428.png" />
+        </div>
+        <div className="rectangle-1432">
+          <img className="rectangle-1428" src="rectangle-1428.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="rectangle-14103"></div>
+  <div className="logo">logo</div>
+  <div className="component-15">
+    <svg
+      className="material-symbols-location-on-outline"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.3334 12.6194C12.879 12.6194 13.3463 12.4249 13.7351 12.036C14.1234 11.6478 14.3175 11.1809 14.3175 10.6352C14.3175 10.0896 14.1234 9.62234 13.7351 9.23345C13.3463 8.84522 12.879 8.65111 12.3334 8.65111C11.7877 8.65111 11.3208 8.84522 10.9326 9.23345C10.5437 9.62234 10.3492 10.0896 10.3492 10.6352C10.3492 11.1809 10.5437 11.6478 10.9326 12.036C11.3208 12.4249 11.7877 12.6194 12.3334 12.6194ZM12.3334 19.911C14.3506 18.0592 15.8469 16.3766 16.8224 14.8634C17.798 13.3508 18.2857 12.0076 18.2857 10.8337C18.2857 9.0314 17.711 7.55554 16.5615 6.40607C15.4127 5.25726 14.0033 4.68286 12.3334 4.68286C10.6634 4.68286 9.25366 5.25726 8.10419 6.40607C6.95538 7.55554 6.38098 9.0314 6.38098 10.8337C6.38098 12.0076 6.86874 13.3508 7.84427 14.8634C8.8198 16.3766 10.3162 18.0592 12.3334 19.911ZM12.3334 22.54C9.67132 20.2748 7.68323 18.1706 6.36907 16.2275C5.05426 14.285 4.39685 12.4871 4.39685 10.8337C4.39685 8.35349 5.1948 6.37763 6.7907 4.90607C8.38594 3.43451 10.2335 2.69873 12.3334 2.69873C14.4332 2.69873 16.2808 3.43451 17.876 4.90607C19.4719 6.37763 20.2699 8.35349 20.2699 10.8337C20.2699 12.4871 19.6128 14.285 18.2986 16.2275C16.9838 18.1706 14.9954 20.2748 12.3334 22.54Z"
+        fill="white"
+      />
+    </svg>
+
+    <div className="hyderabad">Hyderabad</div>
+  </div>
+  <div className="frame-29">
+    <div className="frame-24">
+      <div className="milk">News-Paper</div>
+    </div>
+    <div className="frame-22">
+      <div className="milk">Milk</div>
+    </div>
+    <div className="frame-23">
+      <div className="milk">Curd</div>
+    </div>
+    <div className="frame-28">
+      <div className="milk">Eggs</div>
+    </div>
+    <div className="frame-26">
+      <div className="milk">Vegetables</div>
+    </div>
+    <div className="frame-25">
+      <div className="milk">Breads</div>
+    </div>
+    <div className="frame-27">
+      <div className="milk">Cookies</div>
+    </div>
+  </div>
+</div>
+
+    </>
   )
 }
 
