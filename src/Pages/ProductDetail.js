766364c1dc64724f644b3a47e6ac7072f371a8b5
BHASKAR VVSG <bhaskarvvsg57hds@gmail.com>
2023-09-27 16:08:27 +0530

    added the products page and added Image folder in public

---
+++ src/Pages/ProductDetail.js
@@ -0,0 +1,205 @@
+const ProductDetails =
+[
+    {
+        id:1,
+        Title:"Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.",
+        Cat: 'Tablet',
+        Price: '723',
+        Img: './Images/tp1.jpg'
+    },
+    {
+        id:2,
+        Title:"Tracker with IP67 Waterproof Pedometer Smart watch.",
+        Cat: 'Smart Watch',
+        Price: '168',
+        Img: './Images/tp2.jpg'
+    },
+    {
+        id:3,
+        Title:"Cancelling Headphones Wireless.",
+        Cat: 'Headphone',
+        Price: '49',
+        Img: './Images/tp3.jpg'
+    },
+    {
+        id:4,
+        Title:"Professional Camera 4K Digital Video Camera.",
+        Cat: 'Camera',
+        Price: '1049',
+        Img: './Images/tp4.jpg'
+    },
+    {
+        id:5,
+        Title:"Mini Portable PD 22.5W Fast Charging Powerbank.",
+        Cat: 'Powerbank',
+        Price: '49',
+        Img: './Images/tp5.jpg'
+    },
+    {
+        id:6,
+        Title:"CPU Cooler 2 Heat Pipes 12mm 4 Pin PWM RGB for Intel.",
+        Cat: 'Electronics',
+        Price: '156',
+        Img: './Images/tp6.png'
+    },
+    {
+        id:7,
+        Title:"Playstation 4 2TB Slim Gaming Console.",
+        Cat: 'Gaming',
+        Price: '2098',
+        Img: './Images/tp7.jpg'
+    },
+    {
+        id:8,
+        Title:"Mini Portable Mobile Phone Powerbank for iphone.",
+        Cat: 'Electronics',
+        Price: '386',
+        Img: './Images/tp8.jpg'
+    },
+    {
+        id:9,
+        Title:"Microsoft Surface Pro 8-13' Touchscreen.",
+        Cat: 'Tablet',
+        Price: '693',
+        Img: './Images/p9.jpg'
+    },
+    {
+        id:10,
+        Title:"Playstation 4 2TB Slim Gaming Console.",
+        Cat: 'Gaming',
+        Price: '5036',
+        Img: './Images/p10.jpg'
+    },
+    {
+        id:11,
+        Title:"Echo Show 5 (2nd Gen) Adjustable Stand | Charcoal",
+        Cat: 'Electronics',
+        Price: '198',
+        Img: './Images/p11.jpg'
+    },
+    {
+        id:12,
+        Title:"Echo Dot smart speaker",
+        Cat: 'Electronics',
+        Price: '793',
+        Img: './Images/p12.jpg'
+    },
+];
+
+const ProductDetail =
+[
+    {
+        id:1,
+        Title:"Amul Milk Gold",
+        Cat: 'Milk',
+        Price: '723',
+        Img: './Images/Amul/Amul_Milk_1.png'
+    },
+    {
+        id:2,
+        Title:"Amul Milk Blue",
+        Cat: 'Milk',
+        Price: '168',
+        Img: './Images/Amul/Amul_Milk_2.jpg'
+    },
+    {
+        id:3,
+        Title:"Amul Milk Packet",
+        Cat: 'Milk',
+        Price: '49',
+        Img: './Images/Amul/Amul_Milk_3.jpg'
+    },
+    {
+        id:4,
+        Title:"Heritage Toned Milk",
+        Cat: 'Milk',
+        Price: '1049',
+        Img: './Images/Heritage/Heritage_Milk_1.png'
+    },
+    {
+        id:5,
+        Title:"Heritage Double Toned Milk",
+        Cat: 'Milk',
+        Price: '49',
+        Img: './Images/Heritage/Heritage_Milk_2.jpg'
+    },
+    {
+        id:6,
+        Title:"Hutsun Milk Dairy Whitener",
+        Cat: 'Milk',
+        Price: '156',
+        Img: './Images/Hustun/Hutsun_milk_1.webp'
+    },
+    {
+        id:7,
+        Title:"Amul Cow Ghee Glass Bottle",
+        Cat: 'Ghee',
+        Price: '2098',
+        Img: './Images/Amul/Amul_Cow_Ghee.jpg'
+    },
+    {
+        id:8,
+        Title:"Heritage Cow Ghee packet",
+        Cat: 'Ghee',
+        Price: '386',
+        Img: './Images/Heritage/heritage_cow_ghee.jpg'
+    },
+    {
+        id:9,
+        Title:"Hutsun Ghee Bottle pack",
+        Cat: 'Ghee',
+        Price: '693',
+        Img: './Images/Hustun/Hatsun_Ghee.jpg'
+    },
+    {
+        id:10,
+        Title:"Heritage Total Curd",
+        Cat: 'Curd',
+        Price: '536',
+        Img: './Images/Heritage/Heritage_Curd_1.jpg'
+    },
+    {
+        id:11,
+        Title:"Hutsun Premium Curd",
+        Cat: 'Curd',
+        Price: '198',
+        Img: './Images/Heritage/Heritage_Curd_2.webp'
+    },
+    
+    {
+        id:12,
+        Title:"Heritage Curd",
+        Cat: 'Curd',
+        Price: '793',
+        Img: './Images/Hustun/Hutsun_Curd_2.webp'
+    },
+    {
+        id:13,
+        Title:"Apples",
+        Cat: 'Apple',
+        Price: '793',
+        Img: './Images/Fruits/Apples/Apples.webp'
+    },
+    {
+        id:14,
+        Title:"Banana",
+        Cat: 'Banana',
+        Price: '793',
+        Img: './Images/Fruits/Banana.jpg'
+    },
+    {
+        id:15,
+        Title:"Pineapple",
+        Cat: 'Pineapple',
+        Price: '793',
+        Img: './Images/Fruits/Pineapple.jpg'
+    },
+    {
+        id:16,
+        Title:"Promogranate",
+        Cat: 'Promogranate',
+        Price: '793',
+        Img: './Images/Fruits/Promogranate.png'
+    },
+]
+export default ProductDetail;
