bfedc4d1b4f5a84c7846f9ab68693041dfeb58d2
BHASKAR VVSG <bhaskarvvsg57hds@gmail.com>
2023-10-05 21:22:16 +0530

    products page /products box alignments done  2. subscription popup designing in progress   3. filter the items by brand and category was done in products page

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -6,7 +6,7 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { BsEye } from 'react-icons/bs';
 import Productdetail from '../Pages/ProductDetail';
 import '../Css/Products.css';
-import axios from 'axios';
+import axios, { all } from 'axios';
 import SubscriptionModal  from '../Pages/SubscriptionModal';
 import { useLocation } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
@@ -22,11 +22,15 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
    const [selectedproductPrice, setSelectedproductPrice] = useState([]);
    const [showModal, setShowModal] = useState(false);
    const [selectedProduct, setSelectedProduct] = useState(null);
+   const [selectedCategories, setSelectedCategories] = useState([]);
+   const [ selectedBrands,setSelectedBrands] = useState([]);
+   const [selectedRatings,setSelectedRatings] = useState([]);
+   const [isModalOpen, setIsModalOpen] = useState(false);
    const location = useLocation();
 
 
   const GetAllProducts =()=>{
-    alert("getall products hitted");
+    // alert("getall products hitted");
     const url = "https://localhost:7041/api/Admin/GetAllProducts";
     axios.get(url)
     .then((response)=>{
@@ -54,21 +58,18 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
     
     if (category) 
     {
-      alert("categ if hitted");
       setFilteredProduct(response.data.filter(item => item.categoryName === category));
       setProduct(response.data);
     } 
     else 
     {
       if(brand){
-        alert("brand if hitted");
         setFilteredProduct(response.data.filter(item => item.brandName === brand));
         console.log(filteredProduct);
         setProduct(response.data);
       }
       else
     {
-      alert("laste else hitted");
       setFilteredProduct(response.data);
       setProduct(response.data);
     }}});
@@ -81,16 +82,17 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
       GetAllProducts();
      },[]);
 
-    const handleSubscribeClick = (curElm) => {
-      setSelectedProduct(curElm);
-      console.log(selectedProduct);
-      alert(selectedproductPrice);
+    const handleSubscribeClick = (curElm,selectedPrice) => {
+      setSelectedProduct({ ...curElm, selectedPrice }); // Include selectedPrice in the selectedProduct object
       setShowModal(true);
+      setIsModalOpen(true);
     };
+    console.log(selectedProduct);
 
     const handleCloseModal = () => {
       setShowModal(false);
       setSelectedProduct(null);
+      setIsModalOpen(false);
     };
   
     const filtterproduct = (category) =>
@@ -107,19 +109,60 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
         setFilteredProduct(product);
     }
 
-    
       const handleDropdownChange = (productId, newSize) => {
-        //    const newSize = e.target.value;
-        // setSelectedSize(newSize);
         const selectedProduct = filteredProduct.find(p => p.sizeOfEachUnits.includes(Number(newSize)));
         const selectedPrice = selectedProduct ? selectedProduct.priceOfEachUnits[selectedProduct.sizeOfEachUnits.indexOf(Number(newSize))] : '';
-        setSelectedproductPrice(selectedPrice);
+      
         setSelectedSizes(prevSelectedSizes => ({
           ...prevSelectedSizes,
           [productId]: newSize
         }));
       }
 
+      const handleCategoryChange = (category) => {
+        const updatedCategories = selectedCategories.includes(category)
+          ? selectedCategories.filter(item => item !== category)
+          : [...selectedCategories, category];
+    
+        setSelectedCategories(updatedCategories);
+        filterProducts(updatedCategories, selectedBrands, selectedRatings);
+      }
+
+      const handleBrandChange = (brand) => {
+        const updatedBrands = selectedBrands.includes(brand)
+          ? selectedBrands.filter(item => item !== brand)
+          : [...selectedBrands, brand];
+    
+        setSelectedBrands(updatedBrands);
+        filterProducts(selectedCategories, updatedBrands, selectedRatings);
+      }
+
+      const filterProducts = (categories, brands, ratings) => {
+        let filtered = product;
+        if (categories.length > 0) {
+          filtered = filtered.filter(item => categories.includes(item.categoryName));
+        }
+        if (brands.length > 0) {
+          filtered = filtered.filter(item => brands.includes(item.brandName));
+        }
+        if (ratings.length > 0) {
+          filtered = filtered.filter(item => ratings.includes(item.rating));
+        }
+    
+        setFilteredProduct(filtered);
+      }
+
+      useEffect(() => {
+  if (isModalOpen) {
+    document.body.classList.add('modal-open');
+  } else {
+    document.body.classList.remove('modal-open');
+  }
+
+  return () => {
+    document.body.classList.remove('modal-open');
+  };
+}, [isModalOpen]);
 
 
   return (
@@ -131,27 +174,90 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
             <div className='categories'>
                 <h3>categories</h3>
                 <ul>
-                <li onClick={() =>  AllProducts ()}>All Products</li>
-                    <li onClick={() => filtterproduct ("Milk")}>Milk</li>
-                    <li onClick={() => filtterproduct ("Curd")}>Curd</li>
-                    <li onClick={() => filtterproduct ("Vegetables")}>Vegetables</li>
-                    <li onClick={() => filtterproduct ("Fruits")}>Fruits</li>
-                    <li onClick={() => filtterproduct ("NewsPapers")}>News Papers</li>
-                   
-                </ul>
+                <li>
+                    <input type="checkbox" id="milk" onChange={() => handleCategoryChange("Milk")} />
+                    <label htmlFor="milk">Milk</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="curd" onChange={() => handleCategoryChange("Curd")} />
+                    <label htmlFor="curd">Curd</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="vegetables" onChange={() => handleCategoryChange("Vegetables")} />
+                    <label htmlFor="vegetables">Vegetables</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="fruits" onChange={() => handleCategoryChange("Fruits")} />
+                    <label htmlFor="fruits">Fruits</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="newsPapers" onChange={() => handleCategoryChange("NewsPapers")} />
+                    <label htmlFor="newsPapers">News Papers</label>
+                </li>
+            </ul>
             </div>
             <div className='Brand'>
             <h3>Brand</h3>
             <ul>
-            <li onClick={() =>  AllProducts ()}>All Products</li>
-                <li onClick={() => filtterproduct ("Milk")}>Milk</li>
-                <li onClick={() => filtterproduct ("Curd")}>Curd</li>
-                <li onClick={() => filtterproduct ("Vegetables")}>Vegetables</li>
-                <li onClick={() => filtterproduct ("Fruits")}>Fruits</li>
-                <li onClick={() => filtterproduct ("NewsPapers")}>News Papers</li>
-               
-            </ul>
-        </div>
+         
+            <li>
+                <input type="checkbox" id="milk" onChange={() => handleBrandChange("Heritage")} />
+                <label htmlFor="milk">Heritage</label>
+            </li>
+            <li>
+                <input type="checkbox" id="curd" onChange={() => handleBrandChange("Thirumala")} />
+                <label htmlFor="curd">Thirumala</label>
+            </li>
+            <li>
+                <input type="checkbox" id="vegetables" onChange={() => handleBrandChange("Hindusthan Times")}/>
+                <label htmlFor="vegetables">Hindusthan Times</label>
+            </li>
+            <li>
+                <input type="checkbox" id="fruits" onChange={() => handleBrandChange("Times Of India")} />
+                <label htmlFor="fruits">Times Of India</label>
+            </li>
+            <li>
+                <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("Egg Plant")} />
+                <label htmlFor="newsPapers">Egg Plant</label>
+            </li>
+            <li>
+                <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("Safal")} />
+                <label htmlFor="newsPapers">Safal</label>
+            </li>
+            <li>
+                <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("Milky Mist")} />
+                <label htmlFor="newsPapers">Milky Mist</label>
+             </li>
+            <li>
+                <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("Nestle")} />
+                <label htmlFor="newsPapers">Nestle</label>
+             </li>
+            <li>
+              <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("ITC")} />
+              <label htmlFor="newsPapers">ITC</label>
+            </li>
+            <li>
+              <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("Hatsun")} />
+              <label htmlFor="newsPapers">Hatsun</label>
+            </li>
+            <li>
+              <input type="checkbox" id="newsPapers" onChange={() => handleBrandChange("Nature's Basket")} />
+              <label htmlFor="newsPapers">Nature's Basket</label>
+            </li>
+        </ul>
+            </div>
+        <div className='Rating'>
+        <h3>Rating</h3>
+        <ul>
+        <li onClick={() =>  AllProducts ()}>All Products</li>
+            <li onClick={() => filtterproduct ("Milk")}>Milk</li>
+            <li onClick={() => filtterproduct ("Curd")}>Curd</li>
+            <li onClick={() => filtterproduct ("Vegetables")}>Vegetables</li>
+            <li onClick={() => filtterproduct ("Fruits")}>Fruits</li>
+            <li onClick={() => filtterproduct ("NewsPapers")}>News Papers</li>
+           
+        </ul>
+    </div>
         </div>
         <div className='productbox'>
         <h3 style={{fontSize:'30px', fontWeight:'bolder'}}>All Products  <span style={{ fontSize: '15px', fontWeight:'normal' }} >({filteredProduct.length} results)</span> </h3>
@@ -165,11 +271,11 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
                         ? curElm.priceOfEachUnits[curElm.sizeOfEachUnits.indexOf(Number(selectedSize))]
                         : '';
              
-                      console.log(selectedPrice);
+                      
                         return(
                             <>
                             <Card className='product-card'>
-                            <div className='overlay-icon'>
+                            <div className={`overlay-icon ${isModalOpen ? 'hidden' : ''}`}>
                            <li><AiOutlineHeart className='li-icon' /></li>
                           </div>
                             <Card.Img variant="top" src={`data:image/jpeg;base64,${curElm.base64Image}`} alt={curElm.Title} />
@@ -188,8 +294,8 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
                                       </div>
                               </Card.Text>
                               <div className='product-buttons-div'>
-                              <button className='products-AddToCartBtn' onClick={() => handleSubscribeClick(curElm)}>Add to Cart</button>
-                                <button className='products-SubscribeBtn' onClick={() => handleSubscribeClick(curElm)}>subscribe</button>
+                              <button className='products-AddToCartBtn' >Add to Cart</button>
+                                <button className='products-SubscribeBtn' onClick={() => handleSubscribeClick(curElm,selectedPrice)}>subscribe</button>
                                 </div>
                             </Card.Body>
                           </Card>
@@ -209,7 +315,7 @@ const Products = ({detail, view, close, setClose, addtocart}) => {
 {selectedProduct && (
   <SubscriptionModal
     product={selectedProduct}
-    selectedPrice={selectedproductPrice}
+    Priceofselectedproduct={selectedProduct.selectedPrice}
     handleClose={handleCloseModal}
   />
 )}
--- src/Pages/SubscriptionModal.js
+++ src/Pages/SubscriptionModal.js
@@ -1,7 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import '../Css/SubscriptionModal.css';
+import Card from 'react-bootstrap/Card';
+import { AiOutlineHeart, AiOutlineCloseCircle } from 'react-icons/ai';
 
-const SubscriptionModal = ({ product, selectedPrice, handleClose }) => {
+
+const SubscriptionModal = ({ product,Priceofselectedproduct,subscriptiontypes,handleClose }) => {
+  console.log(product);
+  console.log(Priceofselectedproduct);
+  console.log(product.selectedPrice);
+
   const [subscriptionType, setSubscriptionType] = useState(null);
+  const [selectedproductPrice,setSelectedproductPrice] = useState('');
+  const [selectedSizeofproduct, setSelectedSizeofproduct]= useState([]);
+  const [show,setShow]=useState(true);
+
+
+  useEffect(()=>{ 
+    const selectedSize = product.priceOfEachUnits.includes(Number(Priceofselectedproduct)) ? product.sizeOfEachUnits[product.priceOfEachUnits.indexOf(Number(Priceofselectedproduct))] : "";
+    const selectedPrice = product.sizeOfEachUnits.includes(Number(selectedSize))
+      ? product.priceOfEachUnits[product.sizeOfEachUnits.indexOf(Number(selectedSize))]
+      : '';
+      setSelectedproductPrice(selectedPrice);
+      setSelectedSizeofproduct(selectedSize);
+
+  },[]);
+  console.log(selectedproductPrice);
+  console.log(selectedSizeofproduct);
+
+
 
   const handleSubscribe = () => {
     // Implement your logic for subscribing here
@@ -12,7 +40,71 @@ const SubscriptionModal = ({ product, selectedPrice, handleClose }) => {
     }
   };
 
+  const handleDropdownChange = (productId, newSize) => {
+    const selectedPrice = product ? product.priceOfEachUnits[product.sizeOfEachUnits.indexOf(Number(newSize))] : '';
+    setSelectedproductPrice(selectedPrice);
+    setSelectedSizeofproduct(newSize);
+  }
+
+
+
   return (
+    <>
+
+    <Modal className='susbcriptionmodal'  show={show} onHide={handleClose}>
+    <Modal.Header className='susbcriptionmodal-header' closeButton>
+      <Modal.Title className='susbcriptionmodal-Title'><h2>Select your Subscription</h2></Modal.Title>
+    </Modal.Header>
+    <Modal.Body className='susbcriptionmodal-body'>
+    <div className='productbox-container'>
+  
+                        <Card className='subscriptionmodal-product-card'>
+                        <Card.Body className='product-card-body'>
+                        <div className='productcard-imagediv' >
+                          <img src={`data:image/jpeg;base64,${product.base64Image}`} alt={product.Title}></img>
+                          </div>
+                          <div className='Price-QtyDiv'>
+                          <h2>{product.productName}</h2>
+                          <div className='priceAndSizeDiv'>
+                          <h4>$ {selectedproductPrice}</h4>
+                          {product && (
+                            <select value={selectedSizeofproduct} onChange={(e) => handleDropdownChange(product.productId, e.target.value)} className='QuantitySelectDropdown'>
+                            {product.sizeOfEachUnits.map((size) => (
+                              <option key={size} value={size}>{size} {product.unit}</option>
+                            ))}
+                          </select> )}
+                          </div>
+                          <div className='quantity-incredecre'>
+                          <Button className='btn-decre'>-</Button>
+                          <div className='qtyvaluediv'><p className='qtyvalue'>1</p>
+                          </div>
+                           
+                          <Button className='btn-incre'>+</Button>
+                          </div>
+                          </div>
+                          
+      
+                        </Card.Body>
+                      </Card> 
+                
+              </div>
+      
+    </Modal.Body>
+    <Modal.Footer className='susbcriptionmodal-footer'>
+          <div className='subscriptionModal-btndiv'>
+          <Button className='subscriptionmodal-closeBtn' variant="secondary" onClick={handleClose}>Close </Button>
+            <Button className='subscriptionmodal-Addtocartbtn' onClick={handleSubscribe}>Add to Cart</Button>
+            <Button  className='subscriptionmodal-paynowbtn' onClick={handleSubscribe}>Subscribe</Button>
+          </div>
+      
+      
+    </Modal.Footer>
+  </Modal>
+
+
+
+
+{/*}
     <div className="modal">
       <div className="modal-content">
         <span className="close" onClick={handleClose}>&times;</span>
@@ -35,7 +127,15 @@ const SubscriptionModal = ({ product, selectedPrice, handleClose }) => {
         </div>
         <button onClick={handleSubscribe}>Subscribe</button>
       </div>
-    </div>
+  </div>
+
+                          <Card.Img variant="top" src={`data:image/jpeg;base64,${product.base64Image}`} alt={product.Title} />
+                          <Card.Title className='product-card-title'>{product.productName}</Card.Title>
+
+
+
+*/}
+    </>
   );
 };
 
